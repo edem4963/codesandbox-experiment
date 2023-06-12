9b8be6cff5adebdc08be0d8ea2fa4e8539837c08
edem4963 <112492837+edem4963@users.noreply.github.com>
2023-06-12 19:16:33 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+const welcome = 'Welcome to 30 Days Of React'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const author = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+}
+const date = 'Oct 2, 2020'
+
+// JSX element, header
+const header = (
+  <header>
+    <div className='header-wrapper'>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>Instructor: {author.firstName} {author.lastName}</p>
+      <small>Date: {date}</small>
+    </div>
+  </header>
+)
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech, index) => console.log(<li key={index}>{tech}</li>))
+// JSX element, main
+const main = (
+  <main>
+    <div className='main-wrapper'>
+      <p>
+        Prerequisite to get started{' '}
+        <strong>
+          <em>react.js</em>
+        </strong>
+        :
+      </p>
+      <ul>
+        {techsFormatted}
+      </ul>
+    </div>
+  </main>
+)
+
+// JSX element, footer
+const footer = (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>Copyright 2020</p>
+    </div>
+  </footer>
+)
+
+// JSX element, app
+const app = (
+  <div className='app'>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+
+root.render(
+  app
+);
